5362cef8caf4dacff09428c1fc4095d2cae5b680
George <georgegao888@gmail.com>
2020-07-21 15:02:00 +1000

    july update

--- h5_process.py
+++ h5_process.py
@@ -1,15 +1,22 @@
 from PIL import Image
 import h5py
 import os
+import re
+
+file_dir = '/data/h5/eu_AT130.h5' #relative to this script
+output_dir = '/data/h5/sem_seg/'
+
 cwd = os.getcwd()
 print(cwd)
-save_dir = cwd+'/data/h5/train_img/'
+save_dir = cwd+output_dir
 print(save_dir)
-h5file_in = h5py.File(cwd+'/data/h5/eu_NL333.h5', 'r')
+h5file_in = h5py.File(cwd+file_dir, 'r')
 
 for i in range(len(h5file_in['/train_imgs/'])):
     compound = h5file_in['/train_imgs/'][i]
     nm = str(compound[0])
+    nm = nm.replace('b\'', '') #I am the lord of regex
+    nm = nm.replace('\'', '')
     im = Image.fromarray(compound[1])
-    im.save(save_dir+str(i)+'.png')
+    im.save(save_dir+nm+'.png')
 
